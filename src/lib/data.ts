5ba4b5f7f05647d41b9b34da00301cd8a5059c16
SelfTaughtCoder78704 <bobbynicholson78704@gmail.com>
2023-01-13 08:26:25 -0600

    static render

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -15,31 +15,45 @@ const fakeData = [
   }
 ]
 
+function randomColor() {
+  let color = Math.floor(Math.random() * 16777215).toString(16);
+  return "#" + color;
+}
+
+function makeBgForBars() {
+  let bg = [];
+  for (let i = 0; i < fakeData[0].topics.length; i++) {
+    bg.push(randomColor());
+  }
+  return bg;
+}
+
 export const data = {
   labels: [...fakeData[0].topics],
   datasets: [
     {
       label: '% of respondents',
       data: [...fakeData[0].responses],
-      backgroundColor: [
-        'rgba(255, 134,159,0.4)',
-        'rgba(98,  182, 239,0.4)',
-        'rgba(255, 218, 128,0.4)',
-        'rgba(113, 205, 205,0.4)',
-        'rgba(170, 128, 252,0.4)',
-        'rgba(255, 177, 101,0.4)',
-      ],
+      // backgroundColor: [
+      //   'rgba(255, 134,159,0.4)',
+      //   'rgba(98,  182, 239,0.4)',
+      //   'rgba(255, 218, 128,0.4)',
+      //   'rgba(113, 205, 205,0.4)',
+      //   'rgba(170, 128, 252,0.4)',
+      //   'rgba(255, 177, 101,0.4)',
+      // ],
+      backgroundColor: makeBgForBars(),
       borderWidth: 2,
-      borderColor: [
-        'rgba(255, 134, 159, 1)',
-        'rgba(98,  182, 239, 1)',
-        'rgba(255, 218, 128, 1)',
-        'rgba(113, 205, 205, 1)',
-        'rgba(170, 128, 252, 1)',
-        'rgba(255, 177, 101, 1)',
-      ],
+      // borderColor: [
+      //   'rgba(255, 134, 159, 1)',
+      //   'rgba(98,  182, 239, 1)',
+      //   'rgba(255, 218, 128, 1)',
+      //   'rgba(113, 205, 205, 1)',
+      //   'rgba(170, 128, 252, 1)',
+      //   'rgba(255, 177, 101, 1)',
+      // ],
+      borderColor: makeBgForBars(),
     },
   ],
 };
-
 
